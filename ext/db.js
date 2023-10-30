910e19c5dd35fedaaa80aa633c3b1f28df079f1c
johnpii <roman.shubin2001@mail.ru>
2023-10-30 21:54:05 +0400

    test

--- ext/db.js
+++ ext/db.js
@@ -1,6 +1,6 @@
 const db = require("mongoose");
 require("dotenv").config();
-//Connection strings
+//Connection strings:
 const MONGODB_URI = process.env.MONGO_URL
 //const MONGODB_URI = "mongodb://localhost:27017/lazyhome"
 
