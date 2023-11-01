007f2968d4d2e05918112210810842fa88631f40
Vlad <vlad.lemkov@yandex.ru>
2023-11-01 15:54:25 +0400

    fix: fix error when connecting to db

--- backend/ext/db.js
+++ backend/ext/db.js
@@ -1,9 +1,17 @@
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const db = require("mongoose");
-require("dotenv").config();
 //Connection strings:
 const MONGODB_URI = process.env.MONGO_URL
 //const MONGODB_URI = "mongodb://localhost:27017/lazyhome"
-
-db.connect(MONGODB_URI);
+try{
+    db.connect(MONGODB_URI,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+    console.log('connected to mongodb successfully');
+}catch(e){
+    console.log(e);
+}
 
 module.exports = db;
