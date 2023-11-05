a904d137bd413719b8affaeaaca8ef47c78f921a
Vlad <vlad.lemkov@yandex.ru>
2023-11-05 23:21:39 +0400

    fix(api): add api route

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/api', routes);
 app.use('/api/admin', admin);
 app.use('/api/auth', auth);
 
