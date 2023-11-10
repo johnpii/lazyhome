64cc9998966ade5b47ca358a2150d85b2bee0417
Vlad <vlad.lemkov@yandex.ru>
2023-11-10 20:49:15 +0400

    feat: stylize components

--- frontend/src/components/ItemCard.jsx
+++ frontend/src/components/ItemCard.jsx
@@ -1,16 +1,19 @@
 import { imagefrombuffer } from "imagefrombuffer";
 
-const ItemCard = ({product}) => {
+const ItemCard = ({ product }) => {
   return (
-    <div className='max-w-xs mx-auto max-w-100'>
-      <div>name: {product.title}, price: {product.price}, 
-      image: <img src={imagefrombuffer({
-        type: product.image.type,
-        data: product.image.data,
-      })} alt="item"/> 
-      </div>
+    <div className="max-w-[240px]">
+      <img className="h-60"
+        src={imagefrombuffer({
+          type: product.image.type,
+          data: product.image.data,
+        })}
+        alt="item"
+      />
+      <h3 className="">{product.title}</h3>
+      <p className="">price: {product.price}</p>
     </div>
-  )
-}
+  );
+};
 
-export default ItemCard
+export default ItemCard;
