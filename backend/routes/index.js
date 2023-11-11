fabb2c5d77e7388576ef035211cce4bd9090ddce
johnpii <roman.shubin2001@mail.ru>
2023-11-11 19:56:52 +0400

    add: add some instruments to future registrate and authorize

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,8 +4,6 @@ const router = express.Router();
 const Product = require("../models/product");
 
 router.get('/', async function (req, res) {
-    //const test = new Product({ title: "Lamp", price: 41 });
-    //await test.save();
     const products = await Product.find({});
     res.json({
         title: 'Lazy Home',
