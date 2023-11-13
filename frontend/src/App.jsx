54b520845e5e16b78d2df87217020e4978772725
Vlad <vlad.lemkov@yandex.ru>
2023-11-13 19:39:27 +0400

    feat: add react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,15 @@
-import Home from "./components/Home"
-
-function App() {
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./components/home/Home"
+import ProductDetails from "./components/product-details/ProductDetails"
+const App = () => {
   return (
-    <Home/>
+    <BrowserRouter>
+        <Routes>
+            <Route path="*" element={<Home/>}></Route>
+            <Route path="/" element={<Home/>}></Route>
+            <Route path="/product/:id" element={<ProductDetails/>}></Route>
+        </Routes>
+    </BrowserRouter>
   )
 }
 
