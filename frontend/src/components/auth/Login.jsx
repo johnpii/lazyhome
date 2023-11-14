91d8e26dce832bd34ed077d524c70cad5847c21f
Vlad <vlad.lemkov@yandex.ru>
2023-11-15 00:13:50 +0400

    feat: create Login component

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,41 +1,49 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
 import { useNavigate, Link } from "react-router-dom";
+import { login } from "../../http/post";
 
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const submit = async (e) => {
     e.preventDefault();
-    try {
-      await axios.post("/", {
-        username,
-        password,
-      });
-    } catch (e) {
-      console.log(e);
-    }
   };
 
   return (
     <>
-      <h1>Login</h1>
-      <form action="POST">
-        <label htmlFor="name">Имя пользователя</label>
-        <input
-          type="text"
-          name="nickname"
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <label htmlFor="password">Пароль</label>
-        <input
-          type="password"
-          name="password"
-          text
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <input type="submit" onClick={() => submit()} />
+      <form className="max-w-md h-80 flex flex-col border-2 text-left mx-auto mt-40">
+        <h1 className="text-center mt-10">Регистрация</h1>
+        <div className="mt-10 mx-auto">
+          <input
+            className=""
+            type="text"
+            name="nickname"
+            placeholder="Имя пользователя"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="mt-5 mx-auto">
+          <input
+            type="password"
+            name="password"
+            placeholder="Пароль"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button
+          className="mx-auto mt-3"
+          type="submit"
+          onClick={(e) => {
+            e.preventDefault();
+            login(username, password);
+          }}
+        >
+          Войти
+        </button>
       </form>
+      <p>
+        Нет аккаунта? <Link to="/registration">Зарегистрироваться</Link>
+      </p>
     </>
   );
 };
