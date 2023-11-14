924d24834ff55b6aa6847973632946d66896e62a
Vlad <vlad.lemkov@yandex.ru>
2023-11-15 00:14:12 +0400

    feat: create Registration component

---
+++ frontend/src/components/auth/Registration.jsx
@@ -0,0 +1,45 @@
+import { register } from "../../http/post";
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+
+const Registration = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <>
+      <form className="max-w-md h-80 flex flex-col border-2 text-left mx-auto mt-40">
+        <h1 className="text-center mt-10">Регистрация</h1>
+        <div className="mt-10 mx-auto">
+          <input
+            className=""
+            type="text"
+            name="nickname"
+            placeholder="Имя пользователя"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="mt-5 mx-auto">
+          <input
+            type="password"
+            name="password"
+            placeholder="Пароль"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button
+          className="mx-auto mt-3"
+          type="submit"
+          onClick={(e) => {
+            e.preventDefault();
+            register(username, password);
+          }}
+        >
+          Зарегистрироваться
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default Registration;
