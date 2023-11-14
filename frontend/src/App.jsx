505fd931a7b6f8e1c5442053bff7b5540f7bd920
Vlad <vlad.lemkov@yandex.ru>
2023-11-14 15:36:00 +0400

    feat: add login route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,19 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Home from "./components/home/Home"
-import ProductDetails from "./components/product-details/ProductDetails"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/home/Home";
+import ProductDetails from "./components/product-details/ProductDetails";
+import Login from "./components/auth/Login";
+
 const App = () => {
   return (
     <BrowserRouter>
-        <Routes>
-            <Route path="*" element={<Home/>}></Route>
-            <Route path="/" element={<Home/>}></Route>
-            <Route path="/product/:id" element={<ProductDetails/>}></Route>
-        </Routes>
+      <Routes>
+        <Route path="*" element={<Home />}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/product/:id" element={<ProductDetails />}></Route>
+      </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
