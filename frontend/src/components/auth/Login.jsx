43c56c21f3372f4004affd720662da430460c7c0
Vlad <vlad.lemkov@yandex.ru>
2023-11-15 18:23:13 +0400

    feat: add redirect on login

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -5,11 +5,12 @@ import { login } from "../../http/post";
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
   return (
     <>
       <form className="max-w-md h-80 flex flex-col border-2 text-left mx-auto mt-40">
-        <h1 className="text-center mt-10">Регистрация</h1>
+        <h1 className="text-center mt-10">Вход</h1>
         <div className="mt-10 mx-auto">
           <input
             className=""
@@ -33,14 +34,18 @@ const Login = () => {
           onClick={(e) => {
             e.preventDefault();
             login(username, password);
+            navigate("/");
           }}
         >
           Войти
         </button>
+        <p className="text-center mt-10 ">
+          Нет аккаунта?{" "}
+          <Link to="/registration" className="font-bold">
+            Зарегистрироваться
+          </Link>
+        </p>
       </form>
-      <p>
-        Нет аккаунта? <Link to="/registration">Зарегистрироваться</Link>
-      </p>
     </>
   );
 };
