19111ae364a3ec67a795db5a1de9f4d31c136b45
Vlad <vlad.lemkov@yandex.ru>
2023-11-17 00:40:30 +0400

    feat: create component for cart item

---
+++ frontend/src/components/cart/CartItem.jsx
@@ -0,0 +1,30 @@
+import { imagefrombuffer } from "imagefrombuffer";
+import { Link } from "react-router-dom";
+import { removeItem } from "../../services/cart.service";
+import { formatPrice } from "../../services/price.service";
+const CartItem = ({ cart }) => {
+  return (
+    <div className="flex justify-between py-10 text-2xl font-semibold border-b-2 last:border-0">
+      <div className="w-fit">
+        <Link to={`../product/${cart.product._id}`}>
+          <img
+            className="h-60 w-60"
+            src={imagefrombuffer({
+              type: cart.product.image.type,
+              data: cart.product.image.data,
+            })}
+            alt="item"
+          />
+        </Link>
+      </div>
+      <div className="w-1/4 text-left">{cart.product.title}</div>
+      <div className="">{cart.quantity}</div>
+      <div className="text-left min-w-[100px]">{formatPrice(cart.total)}</div>
+      <div className="text-xl">
+        <button onClick={() => removeItem(cart.product._id)}>Удалить</button>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
