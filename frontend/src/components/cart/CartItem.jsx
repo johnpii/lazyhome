1f5c0299244ecce2883eb6434dec9f36030a5130
Vlad <vlad.lemkov@yandex.ru>
2023-11-17 22:01:06 +0400

    feat: rerender cart when deleting item

--- frontend/src/components/cart/CartItem.jsx
+++ frontend/src/components/cart/CartItem.jsx
@@ -2,7 +2,7 @@ import { imagefrombuffer } from "imagefrombuffer";
 import { Link } from "react-router-dom";
 import { removeItem } from "../../services/cart.service";
 import { formatPrice } from "../../services/price.service";
-const CartItem = ({ cart }) => {
+const CartItem = ({ cart, rerender }) => {
   return (
     <div className="flex justify-between py-10 text-2xl font-semibold border-b-2 last:border-0">
       <div className="w-fit">
@@ -21,7 +21,14 @@ const CartItem = ({ cart }) => {
       <div className="">{cart.quantity}</div>
       <div className="text-left min-w-[100px]">{formatPrice(cart.total)}</div>
       <div className="text-xl">
-        <button onClick={() => removeItem(cart.product._id)}>Удалить</button>
+        <button
+          onClick={() => {
+            removeItem(cart.product._id);
+            rerender.setIsDeleted(!rerender.isDeleted);
+          }}
+        >
+          Удалить
+        </button>
       </div>
     </div>
   );
--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -4,6 +4,7 @@ import CartItem from "../components/cart/CartItem";
 
 const CartScreen = () => {
   const [cart, setCart] = useState({});
+  const [isDeleted, setIsDeleted] = useState(false);
   useEffect(() => {
     const fetchData = async () => {
       const data = await getCart();
@@ -11,7 +12,7 @@ const CartScreen = () => {
       console.log(data); //TODO: remove log
     };
     fetchData();
-  }, []);
+  }, [isDeleted]);
   return (
     <>
       <h1 className="max-w-screen-2xl mx-auto mt-20 text-3xl font-bold text-left">
@@ -19,7 +20,9 @@ const CartScreen = () => {
       </h1>
       <div className="max-w-screen-2xl mx-auto mt-20">
         {cart.length ? (
-          cart.map((item) => <CartItem cart={item} />)
+          cart.map((item) => (
+            <CartItem cart={item} rerender={{ isDeleted, setIsDeleted }} />
+          ))
         ) : (
           <p>Корзина пуста</p>
         )}
