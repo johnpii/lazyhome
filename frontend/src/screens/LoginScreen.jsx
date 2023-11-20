29104e4ddf67039daf1c9e3a5d2c0557c0585a3c
Vlad <vlad.lemkov@yandex.ru>
2023-11-20 19:45:52 +0400

    feat: change style

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -9,10 +9,15 @@ const LoginScreen = () => {
 
   return (
     <>
-      <form className="max-w-md h-80 flex flex-col border-2 text-left mx-auto mt-40">
-        <h1 className="text-center mt-10">Вход</h1>
-        <div className="mt-10 mx-auto">
+      <form
+        className="max-w-md h-80 flex flex-col text-left mx-auto mt-40 
+      border-2 rounded-lg border-cyan-400 py-3 px-6 shadow-xl
+          shadow-cyan-600"
+      >
+        <h1 className="text-center mt-8 text-2xl font-semibold">Вход</h1>
+        <div className="mt-8 mx-auto">
           <input
+            className="border rounded-sm py-1 px-2"
             type="text"
             name="nickname"
             placeholder="Имя пользователя"
@@ -21,6 +26,7 @@ const LoginScreen = () => {
         </div>
         <div className="mt-5 mx-auto">
           <input
+            className="border rounded-sm py-1 px-2"
             type="password"
             name="password"
             placeholder="Пароль"
@@ -28,7 +34,7 @@ const LoginScreen = () => {
           />
         </div>
         <button
-          className="mx-auto mt-3"
+          className="mx-auto mt-3 border-2 rounded-lg border-cyan-400 py-1 px-4"
           type="submit"
           onClick={(e) => {
             e.preventDefault();
@@ -39,9 +45,9 @@ const LoginScreen = () => {
         >
           Войти
         </button>
-        <p className="text-center mt-10 ">
+        <p className="text-center mt-8">
           Нет аккаунта?{" "}
-          <Link to="/registration" className="font-bold">
+          <Link to="/registration" className="font-bold text-cyan-400">
             Зарегистрироваться
           </Link>
         </p>
