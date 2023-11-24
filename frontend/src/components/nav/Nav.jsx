b791ee25fe86e78ea063d0841e9beed9ff627eea
Vlad <vlad.lemkov@yandex.ru>
2023-11-25 02:35:07 +0400

    feat: responsive nav and fix styles

--- frontend/src/components/nav/Nav.jsx
+++ frontend/src/components/nav/Nav.jsx
@@ -1,35 +1,55 @@
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-
+import { ReactComponent as ReactMenu } from "./icons8-menu-w.svg";
+import { ReactComponent as ReactClose } from "./icons8-close-w.svg";
+import { useState } from "react";
 const Nav = () => {
   const isAuthed = useSelector((state) => state.auth.isAuthed);
+  const [open, setOpen] = useState(false);
   const navigation = [
-    { link: "/about", name: "Lazy Home" },
+    { link: "/about", name: "О нас" },
     { link: "/catalog", name: "Каталог" },
     { link: "/cart", name: "Корзина" },
     // { link: "/login", name: "Войти" },
   ];
   return (
-    <div className="max-w-full bg-[#494949] px-4">
-      <ul className="max-w-screen-2xl flex justify-between text-2xl font-bold mx-auto py-8 px-0">
+    <div
+      className="flex justify-between md:flex-col max-w-full bg-[#494949] md:bg-[#494949]
+       md:left-0 w-auto md:w-full 
+       md:transition-all md:duration-500 md:ease-in md:top-0"
+    >
+      <Link to="/catalog">
+        <h1 className="py-8 px-4 text-left text-2xl font-semibold">
+          Lazy Home
+        </h1>
+      </Link>
+      <div
+        onClick={() => setOpen(!open)}
+        className="md:block cursor-pointer hidden md:absolute md:right-8 md:top-6"
+      >
+        {open ? <ReactClose /> : <ReactMenu />}
+      </div>
+      <ul
+        className={`flex text-2xl font-bold py-8 md:flex-col md:text-left md:py-0 static md:absolute
+        md:bg-[#494949] z-auto md:z-[6] w-auto md:w-full 
+       ${
+         open ? "md:top-20 md:opacity-100" : "md:top-[-352px]"
+       } md:opacity-0 opacity-100`}
+      >
         {navigation.map((item) => (
-          <li key={item.link}>
-            <Link to={item.link} className="active:text-cyan-200">
-              {item.name}
-            </Link>
+          <li key={item.link} className="md:my-7 mr-20 md:px-4">
+            <Link to={item.link}>{item.name}</Link>
           </li>
         ))}
         {isAuthed ? (
-          <li>
-            <Link to="/profile" className="active:text-cyan-200">
+          <li className="md:my-7 mr-20 last:mr-0 md:px-4">
+            <Link to="/profile" className="">
               Профиль
             </Link>
           </li>
         ) : (
-          <li>
-            <Link to="/login" className="active:text-cyan-200">
-              Войти
-            </Link>
+          <li className="md:my-7 mr-20 last:mr-0 md:px-4">
+            <Link to="/login">Войти</Link>
           </li>
         )}
       </ul>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      sm: { max: "576px" },
+      // => @media (min-width: 576px) { ... }
+
+      md: { max: "960px" },
+      // => @media (min-width: 960px) { ... }
+
+      hd: { max: "1280px" },
+      // => @media (min-width: 1280px) { ... }
+
+      lg: { max: "1440px" },
+      // => @media (min-width: 1440px) { ... }
+    },
   },
   plugins: [],
-}
+};
